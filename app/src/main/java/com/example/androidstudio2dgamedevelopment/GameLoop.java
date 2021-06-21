d7a2dff8c5d5b383932447cff01be6302a15e20f
ria-leberu <gene.andrei.nadela@gmail.com>
2021-06-21 19:35:47 -0400

    new game objects

---
+++ app/src/main/java/com/example/androidstudio2dgamedevelopment/GameLoop.java
@@ -0,0 +1,16 @@
+package com.example.androidstudio2dgamedevelopment;
+
+import android.view.SurfaceHolder;
+
+public class GameLoop {
+    public GameLoop(Game game, SurfaceHolder surfaceHolder) {
+    }
+
+    public double getAverageUPS() {
+        return 0;
+    }
+
+    public double getAverageFPS() {
+        return 0;
+    }
+}
